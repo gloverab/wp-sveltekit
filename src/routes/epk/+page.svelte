6fee63f51c0b8ca11f082b15ecc6aa9c89313d20
Alex <alexanderbglover@gmail.com>
2022-12-21 15:44:35 -0500

    epk

--- src/routes/epk/+page.svelte
+++ src/routes/epk/+page.svelte
@@ -1,5 +1,4 @@
 <script lang='ts'>
-  import epk from '$assets/wp-epk.pdf'
 
   let wrapperW
 </script>
@@ -9,5 +8,5 @@
 </svelte:head>
 
 <div bind:clientWidth={wrapperW} class='w-full'>
-  <object data={epk} type="application/pdf" width="100%" height="{wrapperW * 1.29411764706}"><a href={epk}>epk.pdf</a></object>
+  <!-- <object data={epk} type="application/pdf" width="100%" height="{wrapperW * 1.29411764706}"><a href={epk}>epk.pdf</a></object> -->
 </div>
