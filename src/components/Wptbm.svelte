064fecbe65a0eb5e42a195cbc9b164ba37d680be
Alex <alexanderbglover@gmail.com>
2023-01-15 21:41:25 -0500

    venmo button

--- src/components/Wptbm.svelte
+++ src/components/Wptbm.svelte
@@ -1,5 +1,19 @@
 <script lang='ts'>
   export let isiOs
+
+  let venmoText = "Buy Tickets via Venmo"
+  let showVenmo = false
+  let animateVenmo = false
+
+  const changeText = async () => {
+    showVenmo = true
+    setTimeout(() => animateVenmo = true)
+  }
+
+  const hideVenmo = async () => {
+    animateVenmo = false
+    setTimeout(() => showVenmo = false, 300)
+  }
 </script>
 
 <div class='w-full border-1 border-grey-light rounded-xl sm:max-w-130 p-4 sm:p-10'>
@@ -18,9 +32,16 @@
     <a class='purchase-button mb-2 bg-green-600 flex justify-center hover:bg-green-500' href='https://40625f.myshopify.com/products/wptbm-kid-a-at-crystal-ballroom-somerville-ma' target='blank'>
       <span class='text-white'>Buy Tickets via Shopify</span>
     </a>
-    <a class='purchase-button bg-blue-600 flex justify-center hover:bg-blue-500' href='{isiOs ? "venmo://paycharge?txn=pay&recipients=weird-phishes&text=Your%20Address" : "intent://paycharge?txn=pay&recipients=weird-phishes#Intent;package=com.venmo;scheme=venmo;end"}' target='blank'>
-      <span class='text-white'>Buy Tickets via Venmo</span>
-    </a>
+    <div class='hidden sm:block'>
+      <button on:click={changeText} class='w-full purchase-button bg-blue-600 flex justify-center hover:bg-blue-500'>
+        <span class='text-white'>{venmoText}</span>
+      </button>
+    </div>
+    <div class='sm:hidden'>
+      <a class='flex purchase-button bg-blue-600 justify-center hover:bg-blue-500' href='{isiOs ? "venmo://paycharge?txn=pay&recipients=weird-phishes&text=Your%20Address" : "intent://paycharge?txn=pay&recipients=weird-phishes#Intent;package=com.venmo;scheme=venmo;end"}' target='blank'>
+        <span class='text-white'>Buy Tickets via Venmo</span>
+      </a>
+    </div>
     <div class='flex justify-center'>
       <span class='text-white text-xs'>Please remember to include your mailing address</span>
     </div>
@@ -31,6 +52,23 @@
     </a>
   </div>
 </div>
+
+{#if showVenmo}
+  <div class='w-screen h-screen flex justify-center items-center z-2 fixed top-0 left-0'>
+    <div on:click={hideVenmo} class='absolute top-0 left-0 h-full w-full bg-black bg-opacity-40 {animateVenmo ? "opacity-100" : "opacity-0"} duration-300' />
+
+    <div class='relative bg-white p-8 rounded-md max-w-100 transform {animateVenmo ? "translate-y-0 opacity-100" : "translate-y-7 opacity-0"} duration-300'>
+      <h2 class='text-xl font-semibold'>It looks like you're on Desktop</h2>
+      <p class='mb-4'>Scan the QR code below with your phone and Venmo us $20 per ticket (@weird-phishes). Please don't forget to include your mailing address in the note.</p>
+      <div class='w-full flex flex-col items-center justify-center'>
+        <div class='w-50 h-50'>
+          <img src="https://www.dropbox.com/s/b1w402d3kg0mahb/venmo-code.jpeg?raw=1" />
+        </div>
+        <span>@weird-phishes on Venmo</span>
+      </div>
+    </div>
+  </div>
+{/if}
 
 <style>
   .purchase-button {
